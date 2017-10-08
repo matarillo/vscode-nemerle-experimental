675d620a732f1892ea92fe7438adb9dbcf3c0286
matarillo <kentaro.inomata@gmail.com>
2017-10-08 13:22:27 -0700

    Test

--- server/TestApp/Program.cs
+++ server/TestApp/Program.cs
@@ -10,6 +10,13 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: TestApp projectDirectoryPath");
+                return;
+            }
+            var engine = NemerleServer.NemerleEngine.LoadFromWorkspace(args[0]);
+            Console.WriteLine("Nemerle Engine is up.");
         }
     }
 }
